25767b4e0622ede258f2c11b0e8019c57a7815f7
CATZ <13862383+GatoAzul-SP@users.noreply.github.com>
2026-03-08 23:56:14 -0400

    Reorganización de estilos y ajustes menores

--- frontend/src/app/accented-button/accented-button.ts
+++ frontend/src/app/accented-button/accented-button.ts
@@ -2,7 +2,7 @@ import { Component, input } from "@angular/core";
 import { RouterLink } from "@angular/router";
 
 @Component({
-	selector: "div.green-button, div.orange-button",
+	selector: "div.primary-button, div.secondary-button",
 	imports: [RouterLink],
 	templateUrl: "./accented-button.html",
 	styleUrl: "./accented-button.css",
